6b0a4f78a574a69db421ab7760b42c2a763d64fb
OmerfarukPazar <ofpazar@gmail.com>
2023-03-21 12:02:33 +0300

    bazı yeni eklemeler

--- WhatsLan.py
+++ WhatsLan.py
@@ -13,43 +13,51 @@
 #FONKSİYONLAR
 uyeler=[]
 def uyeekleme(uyeol:tuple,uyeler:list):
-    uyemail=input("Lütfen Mail Adresinizi Giriniz...")
+    uyemail=input("Lütfen Mail Adresinizi Giriniz...") 
+    mailkontrol(uyemail,uyeler)
     uyeisim=input("Lütfen Oluşturmak İstediğiniz Üye İsminizi Giriniz...")
+
     uyesifre=input("Lütfen Oluşturmak İstediğiniz Üye Sifrenizi Giriniz...")
     uyeol=(uyemail,uyeisim,uyesifre)
     uyeler.append(uyeol)
     print("Başarıyla Kayıt Olundu...")
 
-def sifremiunuttumkontrol(uyemail1,uyeisim1):
-    global uyeler
+def mailkontrol(uyemail,uyeler):
+    for sayac,veri in enumerate(uyeler):
+        if  uyemail in veri[0] :
+            print("Bu mail adresi zaten kayıtlı....")
+            anamenu()
     
-    for i in uyeler:
+def sifremiunuttum(uyemail1,uyeisim1):
+    global uyeler
+    for sayac,veri in enumerate(uyeler):
         #print(i)
-        if uyemail1 == i[0] and uyeisim1 == i[1]:
-            a = list(i)
-            a[2] =input("yeni şifreniz:  ")
-            i = tuple(a)                        #  ÜYELERE YENİ İ VERİSİNİ GEÇİREMEDİM  (İ DÜZENLENDİ)
-            return True  
+        if uyemail1 == veri[0] and uyeisim1 == veri[1]:
+                a = list(veri)
+                a[2] =input("yeni şifreniz:  ")
+                veri = tuple(a)
+                uyeler[sayac]=veri
+                return True                      
     print("Böyle Bir Kullanıcı Yok....")
     return False
-        
 
-#def sifremiunuttum():
 
-    
-
+def anamenu():
+    print("Üye Olmak İçin [1] ********* Şifrenizi Unuttuysanız [2]**********")
+    secim=input("secim: ")
+    if secim =="1" :
+        uyeol=()
+        uyeekleme(uyeol,uyeler)
+    elif secim =="2":
+        print("Hatırladığınız Mail Adresiniz ve Kullanıcı Adınız ? ")
+        uyemail1=input("mail adresiniz: ")
+        uyeisim1=input("üye isminiz: ")
+        sifremiunuttum(uyemail1,uyeisim1)
+        print(uyeler)
 
 
 while True:
-    secim=input("secim: ")
-    if secim =="1" :
-        uyeol=input()
-        uyeekleme(uyeol,uyeler)
-    elif secim =="2":
-        uyemail1=input("mail adresiniz: ")
-        uyeisim1=input("üye isminiz: ")
-        sifremiunuttumkontrol(uyemail1,uyeisim1)
-        print(uyeler)
+    anamenu()
     
 
 
